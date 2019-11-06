149ffc4994567cdb08894d3e898f83f8c69f0800
Hamza <hamza.dahmoun7@gmail.com>
2019-11-06 16:23:08 +0100

    enable/disable saveScoreBtn when username entered

--- end.js
+++ end.js
@@ -1,3 +1,16 @@
+//lets enable the Save button once the user enter a text in the input
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+username.addEventListener("keyup", () =>{
+    //this line of code will enable the Save button
+    //it will give a value to the boolean property "disabled" that is the negation of the boolean value of "username.value"
+    //which is coercion
+    //so if username.value is undefined or null --> booleanOf(null or undefined) is false --> saveScoreBtn.disabled = !false = true
+    //if username.value is undefined or null --> booleanOf(any text) is true --> saveScoreBtn.disabled = !true = false
+    saveScoreBtn.disabled = !username.value;
+});
+
+
 saveHighScore = e =>{
     //this line of code will prevent the button from redirecting us to "/end.html?username="
     //which means it will prevent the button from acting like a submit button that will submit username
