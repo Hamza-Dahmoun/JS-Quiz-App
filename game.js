9892dbb058986c51fdd7f8b378a8f8f12273f6f4
Hamza <hamza.dahmoun7@gmail.com>
2019-11-10 16:10:34 +0100

    moving questions aray from gameJs to questionsJson

--- game.js
+++ game.js
@@ -11,32 +11,7 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];//will contain remaining questions to show
 
-let questions = [
-    {//this is an object representing a question
-        question: "Inside which HTML element do we put the Javascript?",
-        choice1: "<script>",
-        choice2: "<javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1 //this is a property that tells which answer is true
-    },
-    {//this is an object representing a question
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3 //this is a property that tells which answer is true
-    },
-    {//this is an object representing a question
-        question: "How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4 //this is a property that tells which answer is true
-    }
-]; //this is an array of objects
+let questions = []; //this is an array of questions objects ... it will be feeded from questions.json using fetch()
 
 const correctBonus = 10;
 const maxQeustions = 3;
